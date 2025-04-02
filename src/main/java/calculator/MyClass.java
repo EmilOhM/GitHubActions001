1a39d378cfdc96bb7f1f5ce0f54974df61145830
LordPantaloon <emil.melms94@hotmail.com>
2025-04-02 07:46:08 +0200

    Initial commit

---
+++ src/main/java/calculator/MyClass.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class MyClass {
+
+    public int add (int a, int b){
+
+        return a + b;
+    }
+
+    public int sub (int a, int b){
+
+        return a - b;
+    }
+}
