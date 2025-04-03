8d47684691a87f4bd46709dd25adcd69434b77d4
LordPantaloon <emil.melms94@hotmail.com>
2025-04-03 15:39:22 +0200

    update update with error fix

--- src/test/java/calculator/MyClassTest.java
+++ src/test/java/calculator/MyClassTest.java
@@ -10,7 +10,7 @@ public class MyClassTest {
     @Test
     public void testAdd() {
         MyClass myClass = new MyClass();
-        assertEquals(3, myClass.add(1,2 + 1));
+        assertEquals(3, myClass.add(1,2 ));
         assertEquals(0, myClass.add(-1,1));
         assertEquals(0, myClass.add(0,0));
 
